42f101f17a14364114df77326b541b3429050e75
domilx <73660748+domilx@users.noreply.github.com>
2022-01-01 18:26:49 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Minecraft_Add_On_Installer
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.forge = new System.Windows.Forms.Button();
+            this.java = new System.Windows.Forms.Button();
+            this.badlion = new System.Windows.Forms.Button();
+            this.lunar = new System.Windows.Forms.Button();
+            this.curseforge = new System.Windows.Forms.Button();
+            this.closePanel = new System.Windows.Forms.Button();
+            this.optifine = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // forge
+            // 
+            this.forge.BackColor = System.Drawing.Color.White;
+            this.forge.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.forge.Location = new System.Drawing.Point(44, 236);
+            this.forge.Name = "forge";
+            this.forge.Size = new System.Drawing.Size(330, 29);
+            this.forge.TabIndex = 0;
+            this.forge.Text = "Install Forge 1.12.2";
+            this.forge.UseVisualStyleBackColor = false;
+            this.forge.Click += new System.EventHandler(this.forge_Click_1);
+            // 
+            // java
+            // 
+            this.java.BackColor = System.Drawing.Color.White;
+            this.java.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.java.Location = new System.Drawing.Point(44, 207);
+            this.java.Name = "java";
+            this.java.Size = new System.Drawing.Size(330, 29);
+            this.java.TabIndex = 1;
+            this.java.Text = "Install Java 17 SDK";
+            this.java.UseVisualStyleBackColor = false;
+            this.java.Click += new System.EventHandler(this.java_Click_1);
+            // 
+            // badlion
+            // 
+            this.badlion.BackColor = System.Drawing.Color.White;
+            this.badlion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.badlion.Location = new System.Drawing.Point(44, 352);
+            this.badlion.Name = "badlion";
+            this.badlion.Size = new System.Drawing.Size(330, 29);
+            this.badlion.TabIndex = 2;
+            this.badlion.Text = "Install Badlion";
+            this.badlion.UseVisualStyleBackColor = false;
+            this.badlion.Click += new System.EventHandler(this.badlion_Click_1);
+            // 
+            // lunar
+            // 
+            this.lunar.BackColor = System.Drawing.Color.White;
+            this.lunar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lunar.Location = new System.Drawing.Point(44, 294);
+            this.lunar.Name = "lunar";
+            this.lunar.Size = new System.Drawing.Size(330, 29);
+            this.lunar.TabIndex = 3;
+            this.lunar.Text = "Install Lunar";
+            this.lunar.UseVisualStyleBackColor = false;
+            this.lunar.Click += new System.EventHandler(this.lunar_Click_1);
+            // 
+            // curseforge
+            // 
+            this.curseforge.BackColor = System.Drawing.Color.White;
+            this.curseforge.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.curseforge.Location = new System.Drawing.Point(44, 323);
+            this.curseforge.Name = "curseforge";
+            this.curseforge.Size = new System.Drawing.Size(330, 29);
+            this.curseforge.TabIndex = 4;
+            this.curseforge.Text = "Install Curseforge Client";
+            this.curseforge.UseVisualStyleBackColor = false;
+            this.curseforge.Click += new System.EventHandler(this.curseforge_Click_1);
+            // 
+            // closePanel
+            // 
+            this.closePanel.BackColor = System.Drawing.Color.Transparent;
+            this.closePanel.FlatAppearance.BorderSize = 0;
+            this.closePanel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closePanel.Location = new System.Drawing.Point(378, -3);
+            this.closePanel.Name = "closePanel";
+            this.closePanel.Size = new System.Drawing.Size(52, 27);
+            this.closePanel.TabIndex = 6;
+            this.closePanel.UseVisualStyleBackColor = false;
+            this.closePanel.Click += new System.EventHandler(this.closePanel_Click);
+            // 
+            // optifine
+            // 
+            this.optifine.BackColor = System.Drawing.Color.White;
+            this.optifine.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.optifine.Location = new System.Drawing.Point(44, 265);
+            this.optifine.Name = "optifine";
+            this.optifine.Size = new System.Drawing.Size(330, 29);
+            this.optifine.TabIndex = 8;
+            this.optifine.Text = "Install Optifine 1.12.2";
+            this.optifine.UseVisualStyleBackColor = false;
+            this.optifine.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(44, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(330, 142);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(433, 407);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.optifine);
+            this.Controls.Add(this.closePanel);
+            this.Controls.Add(this.curseforge);
+            this.Controls.Add(this.lunar);
+            this.Controls.Add(this.badlion);
+            this.Controls.Add(this.java);
+            this.Controls.Add(this.forge);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+        private Button forge;
+        private Button java;
+        private Button badlion;
+        private Button lunar;
+        private Button curseforge;
+        private Button closePanel;
+        private Button optifine;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+using System.Diagnostics;
+using System;
+using System.Net;
+namespace Minecraft_Add_On_Installer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            if (m.Msg == WM_NCHITTEST)
+                m.Result = (IntPtr)(HT_CAPTION);
+        }
+
+        private const int WM_NCHITTEST = 0x84;
+        private const int HT_CLIENT = 0x1;
+        private const int HT_CAPTION = 0x2;
+
+        private void label1_Click(object sender, EventArgs e)
+		{
+			//ResizeableBorderlessForm resizeableBorderlessForm = new();
+			//resizeableBorderlessForm.Show();
+		}
+
+		private void forge_Click(object sender, EventArgs e)
+		{
+			//Form form = new();
+			//form.Text = "Slider Puzzle Example";
+			//SlidePuzzleControl slidePuzzleControl = new();
+			//slidePuzzleControl.Dock = DockStyle.Fill;
+			//form.Controls.Add(slidePuzzleControl);
+			//form.Show();
+		}
+
+		private void java_Click(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void badlion_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void lunar_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void curseforge_Click(object sender, EventArgs e)
+		{
+
+		}
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void closePanel_Paint(object sender, PaintEventArgs e)
+        {
+			
+        }
+
+        private void closePanel_Click(object sender, EventArgs e)
+        {
+			Close();
+		}
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void movePanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void java_Click_1(object sender, EventArgs e)
+        {
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile("https://download.oracle.com/java/17/latest/jdk-17_windows-x64_bin.exe", @"c:\temp\javaInstaller.exe");
+            Process.Start("C:\\temp\\javaInstaller.exe");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile("https://cdn.discordapp.com/attachments/926539742324068382/926959286804951060/OptiFine_1.12.2_HD_U_G5.jar", @"c:\temp\optifine.jar");
+            Process.Start("java.exe", "-jar c:\\temp\\optifine.jar");
+        }
+
+        private void badlion_Click_1(object sender, EventArgs e)
+        {
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile("https://client-updates-cdn77.badlion.net/Badlion%20Client%20Setup%203.6.4.exe", @"c:\temp\badlion.exe");
+            Process.Start("c:\\temp\\badlion.exe");
+        }
+
+        private void curseforge_Click_1(object sender, EventArgs e)
+        {
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile("https://cdn.discordapp.com/attachments/926539742324068382/926960589090197585/CurseForge_-_Installer.exe", @"c:\temp\curseforge.exe");
+            Process.Start("c:\\temp\\curseforge.exe");
+        }
+
+        private void lunar_Click_1(object sender, EventArgs e)
+        {
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile("https://launcherupdates.lunarclientcdn.com/Lunar%20Client%20v2.9.3.exe", @"c:\temp\lunar.exe");
+            Process.Start("c:\\temp\\lunar.exe");
+        }
+
+        private void forge_Click_1(object sender, EventArgs e)
+        {
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile("https://maven.minecraftforge.net/net/minecraftforge/forge/1.12.2-14.23.5.2860/forge-1.12.2-14.23.5.2860-installer.jar", @"c:\temp\forge.jar");
+            Process.Start("java.exe", "-jar c:\\temp\\forge.jar");
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
